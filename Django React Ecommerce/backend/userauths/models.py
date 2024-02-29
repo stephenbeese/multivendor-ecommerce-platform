56bc20967a11e877b555fad3693811c42eef1e04
Stephen Beese <stephenbeese@stephens-air.home>
2024-02-29 14:08:08 +0000

    Add code to automatically create user profile when a user is created

--- Django React Ecommerce/backend/userauths/models.py
+++ Django React Ecommerce/backend/userauths/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.db.models.signals import post_save
 from shortuuid.django_fields import ShortUUIDField
 
 class User(AbstractUser):
@@ -27,7 +27,7 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
     full_name = models.CharField(max_length=100, null=True, blank=True)
     about = models.TextField(null=True, blank=True)
@@ -48,5 +48,15 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         if self.full_name == "" or self.full_name == None:
             self.full_name = self.user.full_name
+        super(Profile, self).save(*args, **kwargs)
 
-        super(User, self).save(*args, **kwargs)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
