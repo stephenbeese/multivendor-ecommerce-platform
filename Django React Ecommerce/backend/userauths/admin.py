b952dd333c90d2b7480a1062e655424a3049dd25
Stephen Beese <stephenbeese@stephens-air.home>
2024-02-29 15:54:27 +0000

    Configure django admin for User and Profile models

--- Django React Ecommerce/backend/userauths/admin.py
+++ Django React Ecommerce/backend/userauths/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from userauths.models import Profile, User
 
 
-admin.site.register(User)
-admin.site.register(Profile)
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'phone']
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'gender', 'country']
+    search_fields = ['full_name']
+    list_filter = ['date']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
