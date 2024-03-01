808f0fa984bb77b9f47daa7490c9f8cad82bcdb0
Stephen Beese <stephenbeese@Stephens-MacBook-Air.local>
2024-03-01 15:05:15 +0000

    Add API documentation using DRF-YASG

--- Django React Ecommerce/backend/backend/urls.py
+++ Django React Ecommerce/backend/backend/urls.py
@@ -19,10 +19,29 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce Backend APIs",
+        default_version="v1",
+        description="This is the documentation for the backend API",
+        terms_of_service="http://mywebsite.com/policies/",
+        contact=openapi.Contact(email="stephenbeese1@live.co.uk"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes = (permissions.AllowAny, )
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+    
+    # Documentation
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
