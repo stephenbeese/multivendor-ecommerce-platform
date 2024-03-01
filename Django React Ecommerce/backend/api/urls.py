5a8890857764e0cde29eccd09e2e51690676a9d0
Stephen Beese <stephenbeese@Stephens-MacBook-Air.local>
2024-03-01 12:31:49 +0000

    Create register view

--- Django React Ecommerce/backend/api/urls.py
+++ Django React Ecommerce/backend/api/urls.py
@@ -3,5 +3,6 @@ from userauths import views as userauths_views
 
 
 urlpatterns = [
-    path('user/token/', userauths_views.MyTokenObtainPairView.as_view())
+    path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
+    path('user/register/', userauths_views.RegisterView.as_view()),
 ]
--- Django React Ecommerce/backend/userauths/views.py
+++ Django React Ecommerce/backend/userauths/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from userauths.models import User, Profile
 from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
@@ -9,5 +11,8 @@ from userauths.serializer import MyTokenObtainPairSerializer, RegisterSerializer
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-    
 
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = RegisterSerializer
