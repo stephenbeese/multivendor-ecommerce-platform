2c53bd025d2eecfdca680829bdbe7b179f2d39e7
Stephen Beese <stephenbeese@stephens-air.home>
2024-03-02 13:14:31 +0000

    Create login view in React

--- Django React Ecommerce/frontend/src/store/auth.js
+++ Django React Ecommerce/frontend/src/store/auth.js
@@ -1,23 +1,22 @@
 import { create } from "zustand";
 import { mountStoreDevtool } from "simple-zustand-devtools";
 
-const useAuthStore = create((set, get) = > ({
-    allUserData: null,
-    loading: false,
-    
-    user: () => ({
-        user_id: get().allUserData?.user_id || null,
-        username: get().allUserData?.username || null,
-    }),
-    
-    setUser: (user) => set({ allUserData: user }),
-    setLoading: (loading) => set({ loading }),
-    setLoggedIn: () => get().allUserData !== null,
-}))
+const useAuthStore = create((set, get) => ({
+  allUserData: null,
+  loading: false,
 
+  user: () => ({
+    user_id: get().allUserData?.user_id || null,
+    username: get().allUserData?.username || null,
+  }),
 
-if(import.meta.env.DEV){
-    mountStoreDevtool('Store', useAuthStore)
+  setUser: (user) => set({ allUserData: user }),
+  setLoading: (loading) => set({ loading }),
+  isLoggedIn: () => get().allUserData !== null,
+}));
+
+if (import.meta.env.DEV) {
+  mountStoreDevtool("Store", useAuthStore);
 }
 
-export { useAuthStore }
+export { useAuthStore };
---
+++ Django React Ecommerce/frontend/src/views/auth/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { login } from "../../utils/auth";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuthStore } from "../../store/auth";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  console.log(email);
+
+  useEffect(() => {
+    if (isLoggedIn()) {
+      navigate("/");
+    }
+  });
+
+  const resetForm = () => {
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const { error } = login(email, password);
+
+    if (error) {
+      alert(error);
+    } else {
+      navigate("/");
+      resetForm();
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div>
+      <h2>Welcome Back</h2>
+      <p>Login to Continue</p>
+      <form onSubmit={handleLogin}>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
